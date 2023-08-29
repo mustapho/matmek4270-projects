895d671fec8e37c286295efaee40ac85b7aae80a
mustapho <143328060+mustapho@users.noreply.github.com>
2023-08-29 13:35:55 +0000

    Fixing all tests

--- projects/python/mesh_function.py
+++ projects/python/mesh_function.py
@@ -2,9 +2,17 @@ import numpy as np
 
 
 def mesh_function(f, t):
+    ft = np.zeros(len(t))
+    for i in range(len(t)):
+        ft[i] = f(t[i])
+    return ft
     pass
 
 def func(t):
+    if t >= 0 and t <= 3:
+        return(np.exp(-t))
+    elif t <= 4 and t > 3:
+        return(np.exp(-3*t))
     pass
 
 def test_mesh_function():
